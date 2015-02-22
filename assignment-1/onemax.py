9b7d84fd2b0e233926e4f27a161a584ce6ed8377
Guido Zuidhof <me@guido.io>
2015-02-22 15:54:48 +0100

    Assignment 1 ++

--- assignment-1/onemax.py
+++ assignment-1/onemax.py
@@ -2,6 +2,13 @@
 from __future__ import division
 from bitstring import BitArray
 import random
+from itertools import izip
+
+# From http://stackoverflow.com/questions/5389507/iterating-over-every-two-elements-in-a-list
+def pairwise(iterable):
+    "s -> (s0,s1), (s2,s3), (s4, s5), ..."
+    a = iter(iterable)
+    return izip(a, a)
 
 
 def create_population(population_size=100, string_length=25):
@@ -29,12 +36,14 @@ def fitness_stats(population):
 def roulette_size(population):
     return sum( map(fitness, population) )
 
-
 # Generates 2 offspring for parents
-def offspring(parent1, parent2):
+def offspring(parent1, parent2, crossover_probability=0.7):
     
     string_length = len(parent1)
-    crossover_point = random.randint(0, string_length)
+    crossover_point = random.randint(1, string_length-1)
+    
+    if random.random() >= crossover_probability:
+        crossover_point = 0
     
     off1_left  = parent1[:crossover_point]
     off1_right = parent2[crossover_point:]
@@ -45,7 +54,7 @@ def offspring(parent1, parent2):
     offspring_1 = BitArray().join([off1_left, off1_right])
     offspring_2 = BitArray().join([off2_left, off2_right])
     
-    return [offspring_1, offspring_2]
+    return (offspring_1, offspring_2)
 
 
 def mutate(individual, probability):
@@ -54,25 +63,103 @@ def mutate(individual, probability):
     
     #Fill the list of indices to flip bits at
     for index in xrange(len(individual)):
-        if random.random() <= probability:
+        if random.random() < probability:
             bitflip_positions.append(index) 
     
     #Flip the bits at the given positions
     individual.invert(bitflip_positions)
     return individual
     
+# Roulette wheel selection
+def select_parents(population):
+    size = roulette_size(population)
+    
+    #[0.1, 0.3, 0.35, 0.5, 0.7, 1]
+    cumulative = []
+    
+    total = 0    
+    for individual in population:
+        probability = fitness(individual) / size
+        total += probability
+        cumulative.append(total)
+    
+    selected = []
+    amount = len(population)
+
+    for i in xrange(int(amount/2)):
+        selected.append( select_from_cumulative_distribution(cumulative, population))
+    
+    return selected
+    
+def select_from_cumulative_distribution(distribution, population):
+    # Naïve O(N)
+    r = random.random()
+
+    for i, prob in enumerate(distribution):
+        if (prob > r):
+            return population[i]
+            
+        
+    return -1
+    
+    
+def recombine(selected):
+    new_generation = []
+    
+    for parent1, parent2 in pairwise(selected):
+        kid1, kid2 = offspring(parent1, parent2)
+    
+        new_generation.append(kid1)
+        new_generation.append(kid2)
+    
+        
+    
+    return new_generation    
+    
+    
+def ea(iterations=100,  string_length=25):
+    # Initialization
+
+    pop = create_population(population_size=100, string_length=string_length)
+
+    mut_probability = 1/string_length    
+    
+    # Plot
+    stats = []
+    stats.append(fitness_stats(pop))
+    #  
+    
+    for t in xrange(iterations):
+        parents = select_parents(pop)
+        pop = recombine(parents)
+        
+        for individual in pop:
+            mutate(individual, mut_probability)
+        
+        print pop
+        stat = fitness_stats(pop)
+        print stat
+        stats.append(stat)
+        
+    
+    
 if __name__ == '__main__':
     
-    pop = create_population(10, string_length=8)
-    for x in pop:
-        print x.bin   
+    ea()
+    
+    
+    #pop = create_population(10, string_length=8)
+    #for x in pop:
+    #    print x.bin   
     
     #print fitness(pop[0])
     #print roulette_size(pop)
     #print fitness_stats(pop)  
 
-    p1 = BitArray('0b111111')
-    p2 = BitArray('0b000000')
-    print offspring(p1, p2)
+    #p1 = BitArray('0b111111')
+    #p2 = BitArray('0b000000')
+    #print offspring(p1, p2)
     
-    print mutate(p1, 0.25)
+    #print select_parents(pop)    
+    
+   # print mutate(p1, 0.25)
